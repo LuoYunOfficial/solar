4be641176f35670874ff3e3bb8446713ff7c5573
LuoYunOfficial <134815865+LuoYunOfficial@users.noreply.github.com>
2025-07-20 16:34:09 +0800

    Create main.js

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+let script = document.createElement('script');
+script.src = "https://luoyunofficial.github.io/solar/assets/inde.js"; // 没有这个脚本
+document.head.append(script);
+
+script.onerror = function() {
+  
+	    // 获取父元素
+var parent = document.getElementById("root-container");
+
+// 创建新元素
+var newElement = document.createElement("t2");
+newElement.innerHTML = "It's not your fault, but our problem.";
+var newElementTwo = document.createElement("p");
+newElementTwo.innerHTML = "An error occurred when loading configurations, please try again later.";
+
+// 插入新元素
+parent.appendChild(newElement);
+parent.appendChild(newElementTwo);
+};
