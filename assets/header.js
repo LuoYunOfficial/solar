fa7af3e0935da54dc48bdd2d8b743487c98c210c
LuoYunOfficial <134815865+LuoYunOfficial@users.noreply.github.com>
2025-07-20 16:33:57 +0800

    Update header.js

--- assets/header.js
+++ assets/header.js
@@ -106,24 +106,3 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-// Script from the end of the original index.html
-let script = document.createElement('script');
-script.src = "https://luoyunofficial.github.io/solar/assets/in.js"; // 没有这个脚本
-document.head.append(script);
-
-script.onerror = function() {
-  
-	    // 获取父元素
-var parent = document.getElementById("root-container");
-
-// 创建新元素
-var newElement = document.createElement("t2");
-newElement.innerHTML = "It's not your fault, but our problem.";
-var newElementTwo = document.createElement("p");
-newElement.innerHTML = "An error occurred when loading configurations, please try again later.";
-
-// 插入新元素
-parent.appendChild(newElement);
-parent.appendChild(newElementTwo);
-};
-
