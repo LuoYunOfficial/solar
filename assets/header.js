c5c9b4b44670e9eb4bab035ad467626ae21c39bc
LuoYunOfficial <134815865+LuoYunOfficial@users.noreply.github.com>
2025-08-07 15:10:15 +0800

    Update header.js

--- assets/header.js
+++ assets/header.js
@@ -50,9 +50,11 @@ document.addEventListener("DOMContentLoaded", function() {
                             </button>
                         </div>
                         <div class="m-hidden">
-                            <button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
-                                <p class="lang" key="LOGIN">Login</p>
-                            </button>
+                            <a href="https://luoyunofficial.github.io/solar/login">
+                                <button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
+                                    <p class="lang" key="LOGIN">Login</p>
+                                </button>
+                            </a>
                         </div>
                         <button class="mobile-menu-button md:hidden" id="mobile-menu-button">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
